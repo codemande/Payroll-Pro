9d50bc13988f9ff0f27c254d70f0859313a2eb41
codemande <1o0xday.2a10@gmail.com>
2026-04-14 08:51:09 +0100

    feat(server): create express server, configure MongoDB connection, install dotenv and dependencies

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import express from "express";
+import connectDB from "./config/db.js"
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+connectDB();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
