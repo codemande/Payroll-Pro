5b3ccaf2645bd576dd3245d9308a9dde7a199bdd
codemande <1o0xday.2a10@gmail.com>
2026-04-14 22:46:55 +0100

    fix(middleware): add error handling to isAdmin middleware

--- server/src/middleware/adminMiddleware.js
+++ server/src/middleware/adminMiddleware.js
@@ -2,16 +2,20 @@ import Role from "../models/Role.model.js";
 
 const isAdmin = async (req, res, next) => {
 
-  const role = await Role.findOne({
-    user: req.user.id,
-    role: "admin"
-  });
+  try {
+    const role = await Role.findOne({
+      user: req.user.id,
+      role: "admin"
+    });
 
-  if (!role) {
-    return res.status(403).json({ message: "Admin Only" });
+    if (!role) {
+      return res.status(403).json({ message: "Admin Only" });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
-
-  next();
 };
 
 export default isAdmin;
