daaf250318900a76929652aac2a3d6881b1ad933
codemande <1o0xday.2a10@gmail.com>
2026-04-14 19:43:01 +0100

    feat(server):  register employee routes

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,15 @@
 import 'dotenv/config'
 import express from "express";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
+import employeeRoutes from "./routes/employeeRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/employees", employeeRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
