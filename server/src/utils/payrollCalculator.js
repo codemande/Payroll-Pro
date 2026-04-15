7fdeeaee727b6b81418b754596fae4d0a3f7b06d
codemande <1o0xday.2a10@gmail.com>
2026-04-15 09:13:31 +0100

    feat(payroll): add payroll calculation logic with tax and deductions

---
+++ server/src/utils/payrollCalculator.js
@@ -0,0 +1,30 @@
+import EmployeeDeduction from "../models/EmployeeDeduction.model.js";
+
+const calculatePayroll = async (employee) => {
+
+  let baseSalary = employee.baseSalary;
+
+  let tax = (baseSalary * employee.taxRate) / 100;
+
+  const deductions = await EmployeeDeduction.findOne({
+    employee: employee._id,
+    isActive: true
+  });
+
+  let deductionTotal = 0;
+
+  deductions.forEach(d => {
+    deductionTotal += (baseSalary * d.percentage) / 100;
+  });
+
+  const netSalary = baseSalary - tax - deductionTotal;
+
+  return{
+    baseSalary,
+    tax,
+    deductions: deductionTotal,
+    netSalary,
+  }
+};
+
+export { calculatePayroll }
