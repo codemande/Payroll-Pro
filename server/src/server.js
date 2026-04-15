124f999b2ac616f1d899b49d356566b3e066f1f8
codemande <1o0xday.2a10@gmail.com>
2026-04-15 10:20:05 +0100

    feat(server): enable cors and register auth, position, and payroll routes

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,23 @@
 import 'dotenv/config'
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import positionRoutes from "./routes/positionRoutes.js";
+import payrollRoutes from "./routes/payrollRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
+app.use("/api/positions", positionRoutes);
+app.use("/api/payroll", payrollRoutes);
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
