f1abf1564b4c2f7247a201b25100ced686ec52c5
codemande <1o0xday.2a10@gmail.com>
2026-04-16 18:16:14 +0100

    feat(hooks): Create custom hook to detect mobile screen size using window resize

---
+++ client/src/hooks/useMobile.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth < MOBILE_BREAKPOINT
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+}
