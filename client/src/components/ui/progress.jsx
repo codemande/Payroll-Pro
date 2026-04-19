481a59854944fa477a9df781a7e19921a8f6be72
codemande <1o0xday.2a10@gmail.com>
2026-04-19 23:59:52 +0100

    style: refactor Progress UI component to external CSS

--- client/src/components/ui/progress.jsx
+++ client/src/components/ui/progress.jsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
+import "../../styles/progress.css";
 
-import { cn } from "../../lib/utils";
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-      className
-    )}
+    className={cn("progress-root", className)}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="progress-indicator"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
