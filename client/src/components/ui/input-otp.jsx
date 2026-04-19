6eb4a641a0094169b2785d1efab76e2a530c0cd9
codemande <1o0xday.2a10@gmail.com>
2026-04-19 23:50:22 +0100

    style: refactor InputOTP UI component to external CSS

--- client/src/components/ui/input-otp.jsx
+++ client/src/components/ui/input-otp.jsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import { OTPInput, OTPInputContext } from "input-otp";
 import { Dot } from "lucide-react";
 
-import { cn } from "../../lib/utils";
+import "../../styles/input-otp.css";
+
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const InputOTP = React.forwardRef(
   ({ className, containerClassName, ...props }, ref) => (
     <OTPInput
       ref={ref}
-      containerClassName={cn(
-        "flex items-center gap-2 has-[:disabled]:opacity-50",
-        containerClassName
-      )}
-      className={cn("disabled:cursor-not-allowed", className)}
+      containerClassName={cn("otp-container", containerClassName)}
+      className={cn("otp-input", className)}
       {...props}
     />
   )
@@ -20,7 +21,7 @@ const InputOTP = React.forwardRef(
 InputOTP.displayName = "InputOTP";
 
 const InputOTPGroup = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("flex items-center", className)} {...props} />
+  <div ref={ref} className={cn("otp-group", className)} {...props} />
 ));
 InputOTPGroup.displayName = "InputOTPGroup";
 
@@ -31,17 +32,13 @@ const InputOTPSlot = React.forwardRef(({ index, className, ...props }, ref) => {
   return (
     <div
       ref={ref}
-      className={cn(
-        "relative flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
-        isActive && "z-10 ring-2 ring-ring ring-offset-background",
-        className
-      )}
+      className={cn("otp-slot", isActive && "is-active", className)}
       {...props}
     >
       {char}
       {hasFakeCaret && (
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="animate-caret-blink h-4 w-px bg-foreground duration-1000" />
+        <div className="otp-caret-wrapper">
+          <div className="otp-caret" />
         </div>
       )}
     </div>
@@ -50,7 +47,7 @@ const InputOTPSlot = React.forwardRef(({ index, className, ...props }, ref) => {
 InputOTPSlot.displayName = "InputOTPSlot";
 
 const InputOTPSeparator = React.forwardRef(({ ...props }, ref) => (
-  <div ref={ref} role="separator" {...props}>
+  <div ref={ref} role="separator" className="otp-separator" {...props}>
     <Dot />
   </div>
 ));
