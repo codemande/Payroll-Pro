00fcd30c460d1ae6e9854671160cf974486ca950
codemande <1o0xday.2a10@gmail.com>
2026-04-19 23:18:18 +0100

    style: refactor Alert UI component to external CSS

--- client/src/components/ui/alert.jsx
+++ client/src/components/ui/alert.jsx
@@ -1,35 +1,35 @@
 import * as React from "react";
-import { cva } from "class-variance-authority";
+import "../../styles/alert.css";
 
-import { cn } from "../../lib/utils";
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
-const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
-  {
-    variants: {
-      variant: {
-        default: "bg-background text-foreground",
-        destructive: "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  },
-);
-
-const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
-  <div ref={ref} role="alert" className={cn(alertVariants({ variant }), className)} {...props} />
+const Alert = React.forwardRef(({ className, variant = "default", ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn("alert", `alert-${variant}`, className)}
+    {...props}
+  />
 ));
 Alert.displayName = "Alert";
 
 const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h5 ref={ref} className={cn("mb-1 font-medium leading-none tracking-tight", className)} {...props} />
+  <h5
+    ref={ref}
+    className={cn("alert-title", className)}
+    {...props}
+  />
 ));
 AlertTitle.displayName = "AlertTitle";
 
 const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("text-sm [&_p]:leading-relaxed", className)} {...props} />
+  <div
+    ref={ref}
+    className={cn("alert-description", className)}
+    {...props}
+  />
 ));
 AlertDescription.displayName = "AlertDescription";
 
