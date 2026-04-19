13ee9a1571836f1825d3b677e3239272c666150b
codemande <1o0xday.2a10@gmail.com>
2026-04-19 23:45:00 +0100

    style: refactor Drawer UI component to external CSS

--- client/src/components/ui/drawer.jsx
+++ client/src/components/ui/drawer.jsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 
-import { cn } from "../../lib/utils";
+import "../../styles/drawer.css";
+
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Drawer = ({ shouldScaleBackground = true, ...props }) => (
   <DrawerPrimitive.Root shouldScaleBackground={shouldScaleBackground} {...props} />
@@ -15,7 +19,11 @@ const DrawerPortal = DrawerPrimitive.Portal;
 const DrawerClose = DrawerPrimitive.Close;
 
 const DrawerOverlay = React.forwardRef(({ className, ...props }, ref) => (
-  <DrawerPrimitive.Overlay ref={ref} className={cn("fixed inset-0 z-50 bg-black/80", className)} {...props} />
+  <DrawerPrimitive.Overlay 
+    ref={ref} 
+    className={cn("drawer-overlay", className)} 
+    {...props} 
+  />
 ));
 DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
 
@@ -24,13 +32,10 @@ const DrawerContent = React.forwardRef(({ className, children, ...props }, ref) 
     <DrawerOverlay />
     <DrawerPrimitive.Content
       ref={ref}
-      className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
-        className,
-      )}
+      className={cn("drawer-content", className)}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="drawer-handle" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
@@ -38,26 +43,30 @@ const DrawerContent = React.forwardRef(({ className, children, ...props }, ref) 
 DrawerContent.displayName = "DrawerContent";
 
 const DrawerHeader = ({ className, ...props }) => (
-  <div className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)} {...props} />
+  <div className={cn("drawer-header", className)} {...props} />
 );
 DrawerHeader.displayName = "DrawerHeader";
 
 const DrawerFooter = ({ className, ...props }) => (
-  <div className={cn("mt-auto flex flex-col gap-2 p-4", className)} {...props} />
+  <div className={cn("drawer-footer", className)} {...props} />
 );
 DrawerFooter.displayName = "DrawerFooter";
 
 const DrawerTitle = React.forwardRef(({ className, ...props }, ref) => (
   <DrawerPrimitive.Title
     ref={ref}
-    className={cn("text-lg font-semibold leading-none tracking-tight", className)}
+    className={cn("drawer-title", className)}
     {...props}
   />
 ));
 DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
 
 const DrawerDescription = React.forwardRef(({ className, ...props }, ref) => (
-  <DrawerPrimitive.Description ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props} />
+  <DrawerPrimitive.Description 
+    ref={ref} 
+    className={cn("drawer-description", className)} 
+    {...props} 
+  />
 ));
 DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
 
