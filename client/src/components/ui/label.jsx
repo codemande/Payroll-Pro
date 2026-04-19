81d3bac55f5992a0fe8eaeebaaf4c456cfd1406f
codemande <1o0xday.2a10@gmail.com>
2026-04-19 23:51:42 +0100

    style: refactor Label UI component to external CSS

--- client/src/components/ui/label.jsx
+++ client/src/components/ui/label.jsx
@@ -1,17 +1,15 @@
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cva } from "class-variance-authority";
+import "../../styles/label.css";
 
-import { cn } from "../../lib/utils";
-
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-);
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn("label", className)}
     {...props}
   />
 ));
